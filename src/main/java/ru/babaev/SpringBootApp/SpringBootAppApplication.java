508cfbc249cbbba5e1fd916727d8e8ef1d8aab05
Babaev Sergei <rutrackerorg2004@mail.ru>
2024-02-19 20:06:42 +0300

    first commit in first_branch

--- src/main/java/ru/babaev/SpringBootApp/SpringBootAppApplication.java
+++ src/main/java/ru/babaev/SpringBootApp/SpringBootAppApplication.java
@@ -15,6 +15,7 @@ public class SpringBootAppApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootAppApplication.class, args);
+		System.out.println("here");
 	}
 
 	@Bean
